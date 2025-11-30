216cc1f3d6f9cfa895c006abb688c3bb60347ed9
Muzdalifah <muzdalifahsuliman@gmail.com>
2025-11-30 11:50:36 +0200

    simple quiz with js

---
+++ quiz.js
@@ -0,0 +1,17 @@
+function checkAnswer() {
+  const feedback = document.getElementById("feedback");
+  const correctAnswer = 4;
+  const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+
+  if (userAnswer == correctAnswer) {
+    feedback.textContent = "Correct! Well done.";
+    feedback.style.color = "#28a745";
+  } else {
+    feedback.textContent = "That's incorrect. Try again!";
+    feedback.style.color = "#dc3545";
+  }
+}
+
+const submitAnswer = document.getElementById("submit-answer");
+
+submitAnswer.addEventListener("click", checkAnswer);
